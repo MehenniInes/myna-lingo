95bbf56d2eb4df46fe6ea1a486a67deed3693e21
ines <mehenni914519@gmail.com>
2026-09-20 15:49:00 +0100

    Enable CORS for frontend origin

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(4000);
 }
